745c0f01884549c3f49a4e6b9230ab8697b33fc9
WadaTakafumi <wdtk2525@gmail.com>
2021-07-01 11:24:08 +0900

    Add: Routerの整備、簡単なモックアップ作成

---
+++ src/components/Header.vue
@@ -0,0 +1,38 @@
+<template>
+  <header>
+    <div class="icon__container" @click="linkToTop">
+      <div class="icon__content">WikiQuiz</div>
+    </div>
+  </header>
+</template>
+
+<script>
+export default {
+  methods: {
+    linkToTop() {
+      if (this.$router.currentRoute.name === "Home") return;
+      this.$router.push("/");
+    },
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+.icon {
+  &__container {
+    position: fixed;
+    top: 0;
+    left: 0;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    z-index: 99;
+    background-color: rgba(248, 247, 247, 0.5);
+    cursor: pointer;
+  }
+  &__content {
+    margin: 5px 10px;
+    font-size: 30px;
+  }
+}
+</style>
---
+++ src/views/Home.vue
@@ -0,0 +1,72 @@
+<template>
+  <div>
+    <!-- <div><button @click="linkToMain">WikiQuizに挑む</button></div> -->
+    <section class="top">
+      <div class="top__topic">
+        <div class="top__topic--main">WikiQuiz</div>
+        <div class="top__topic--sub">
+          <div>text text text text text text</div>
+          <div>text text text text text text</div>
+          <div>text text text</div>
+        </div>
+      </div>
+    </section>
+    <section class="about">
+      <div class="about__topic">About</div>
+      <div class="about__contents">
+        <div class="about__text">
+          <div class="about__text--main">text text text text text text</div>
+          <div class="about__text--sub">
+            <div>text text text text text</div>
+            <div>text text text text</div>
+            <div>text text text text text</div>
+          </div>
+          <div class="about__text--sub">
+            <div>text text text text text</div>
+            <div>text text text text</div>
+            <div>text text text text text</div>
+          </div>
+          <div class="about__button"><button>more</button></div>
+        </div>
+        <div class="about__image"><img src="" alt="" /></div>
+      </div>
+    </section>
+    <section class="howto"></section>
+  </div>
+</template>
+
+<script>
+export default {
+  methods: {
+    linkToMain() {
+      this.$router.push("/main");
+    },
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+.top {
+  height: 100vh;
+  width: 100vw;
+  background-color: aliceblue;
+  &__topic {
+    position: absolute;
+    top: 50%;
+    left: 10%;
+    &--main {
+      margin: 20px 0;
+      font-size: 5em;
+      font-weight: 600;
+    }
+    &--sub {
+      font-size: 1.2em;
+    }
+  }
+}
+.about {
+  height: 90vh;
+  width: 100vw;
+  background-color: beige;
+}
+</style>
